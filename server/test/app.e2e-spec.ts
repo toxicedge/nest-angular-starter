7a9c14b5e56e68921de45489e5813bb456b2538e
Chris Waymouth <chris@toxicedge.com>
2020-02-11 19:47:07 -0500

    fix server test scripts

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('ApiController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,9 +15,9 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/api/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api/')
       .expect(200)
       .expect('Hello World!');
   });
